1a7093dfc32c4be08cfb175e241f00d0afa75b76
Kate Bryan <katebryan31@gmail.com>
2020-08-29 07:26:45 +0100

    feat: services section added

--- src/components/Services.js
+++ src/components/Services.js
@@ -6,6 +6,20 @@ export const Services = () => {
   return (
     <section className="section bg-grey">
       <Title title="services" />
+      <div className="section-center services-center">
+        {services.map(service => {
+          const { id, icon, title, text } = service
+
+          return (
+            <article key={id} className="service">
+              {icon}
+              <h4>{title}</h4>
+              <div className="underline"></div>
+              <p>{text}</p>
+            </article>
+          )
+        })}
+      </div>
     </section>
   )
 }
