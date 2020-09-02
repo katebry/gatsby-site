2d9673a8f95b346cde131b029a94b7df1dd35f35
Kate Bryan <katebryan31@gmail.com>
2020-09-02 07:09:44 +0100

    feat: padding out Projects page

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,1 +1,12 @@
 import React from "react"
+import PropTypes from "prop-types"
+import Image from "gatsby-image"
+import { FaGithub, FaHome } from "react-icons/fa"
+
+export const Project = () => {
+  return (
+    <>
+      <p>Project</p>
+    </>
+  )
+}
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,22 @@
 import React from "react"
+import { Title } from "./Title"
+import { Project } from "./Project"
+import { Link } from "gatsby"
 
-export const Projects = () => {
+export const Projects = ({ projects, title, showLink }) => {
   return (
-    <>
-      <p>Projects page</p>
-    </>
+    <section className="section projects">
+      <Title title={title} />
+      <div className="section-center projects-center">
+        {projects.map((project, index) => {
+          return <Project key={project.id} index={index} {...project} />
+        })}
+      </div>
+      {showLink ? (
+        <Link to="/projects" className="btn center-btn">
+          Projects
+        </Link>
+      ) : null}
+    </section>
   )
 }
