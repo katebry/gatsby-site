d546db5a579d0296884642cd88e5f14322149bbd
Kate Bryan <katebryan31@gmail.com>
2020-09-02 06:30:27 +0100

    chore: beginning of project section setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,49 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import { Hero } from "../components/Hero"
 import { Services } from "../components/Services"
 import { Jobs } from "../components/Jobs"
+import { Projects } from "../components/Projects"
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const {
+    allStrapiProjects: { nodes: projects },
+  } = data
   return (
     <>
       <Layout>
         <Hero />
         <Services />
         <Jobs />
+        <Projects projects={projects} title="Featured Projects" showLink />
       </Layout>
     </>
   )
 }
+
+export const query = graphql`
+  {
+    allStrapiProjects(filter: { featured: { eq: true } }) {
+      nodes {
+        github
+        id
+        description
+        title
+        url
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
