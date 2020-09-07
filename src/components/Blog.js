e34f4c140f77436172f54e36952b2522ac79ec48
Kate Bryan <katebryan31@gmail.com>
2020-09-07 06:43:14 +0100

    feat: handling individual blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,29 @@
 import React from "react"
+import { Link } from "gatsby"
+import Image from "gatsby-image"
 
-export const Blog = () => {
+export const Blog = ({
+  id,
+  title,
+  image,
+  date,
+  category,
+  slug,
+  description,
+}) => {
   return (
-    <>
-      <h1> A single blog </h1>
-    </>
+    <Link to={`/blogs/${slug}`} className="blog" key={id}>
+      <article>
+        <Image fluid={image.childImageSharp.fluid} className="blog-img" />
+        <div className="blog-card">
+          <h4>{title}</h4>
+          <p>{description}</p>
+          <div className="blog-footer">
+            <p>{category}</p>
+            <p>{date}</p>
+          </div>
+        </div>
+      </article>
+    </Link>
   )
 }
