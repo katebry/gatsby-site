b01d36e8271a8394ffebe58cb61ee46c7b24ba0f
Kate Bryan <katebryan31@gmail.com>
2020-10-31 13:09:03 +0000

    chore: added Footer to projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Layout } from "../components/Layout"
 import { graphql } from "gatsby"
 import { Projects } from "../components/Projects"
+import { Footer } from "../components/Footer"
 
 const ProjectsPage = ({
   data: {
@@ -13,6 +14,7 @@ const ProjectsPage = ({
       <section className="projects-page">
         <Projects projects={projects} title="all projects" />
       </section>
+      <Footer />
     </Layout>
   )
 }
